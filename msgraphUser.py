64208ba2947a8662ab849f5a5bfb5c58cc7549b2
WONSZ <mf.fliszkiewicz@gmail.com>
2023-03-28 15:43:52 +0200

    #3

--- msgraphUser.py
+++ msgraphUser.py
@@ -7,10 +7,10 @@ import json
 SCOPES = ["https://graph.microsoft.com/.default"]
 
 # The App Registration's application (client) ID
-client_id = 'x'
+client_id = '12981ac8-3f9b-48b0-9839-918ecebbca70'
 
 # Your Azure AD tenant ID
-tenant_id = 'x'
+tenant_id = 'ea7b03ca-00db-4c19-9b8c-6442ab16a46f'
 
 ENDPOINT_URI = 'https://graph.microsoft.com/beta/'
 
@@ -78,20 +78,18 @@ def listAllConfigs():
 
 def get_specific_config(config_id):
     request = requests.get(url=ENDPOINT_URI+f'/deviceManagement/configurationPolicies/{config_id}', headers=req_headers)
-    print(request)
-    print(json.dumps(request.json(), indent=5, ensure_ascii=False))
+    return request.json()
 
 def get_specific_config_settings(config_id):
     request = requests.get(url=ENDPOINT_URI+f'/deviceManagement/configurationPolicies/{config_id}/settings', headers=req_headers)
     #print(request)
     #print(json.dumps(request.json(), indent=5, ensure_ascii=False))
-    return request.json()['value']
+    return request.json()
 
 
 def create_specific_config(json):
     request = requests.post(url=ENDPOINT_URI+f'/deviceManagement/configurationPolicies/', headers=req_headers, json=json)
-    print(request)
-    print(request.content)
+    print(request.json())
 
 test_json = {
      "@odata.context": "https://graph.microsoft.com/beta/$metadata#deviceManagement/configurationPolicies/$entity",
@@ -147,11 +145,21 @@ test_json = {
                      }
                  ]
 }
+
+config_id = '3bd139ed-66b3-4afc-975c-dc81eae1f2b8'
 #createNewUser(user_json)
 #listAllUsers()
 #listAllConfigs()
-get_specific_config('b553a081-4cce-4ef9-bda5-8f2df5172ca8')
-qwerty = get_specific_config_settings('b553a081-4cce-4ef9-bda5-8f2df5172ca8')
-print(json.dumps(qwerty, indent=5, ensure_ascii=False))
+policy_json = get_specific_config(config_id)
+policy_json_settings = get_specific_config_settings(config_id)['value']
+print(json.dumps(policy_json, indent=5, ensure_ascii=False))
+print(json.dumps(policy_json_settings, indent=5, ensure_ascii=False))
 
-create_specific_config(test_json)
+policy_json['settings'] = policy_json_settings
+policy_json['name'] = 'new_awsome_name'
+print(json.dumps(policy_json,indent=10,ensure_ascii=False))
+
+
+#print(merged_json)
+create_specific_config(policy_json)
+
